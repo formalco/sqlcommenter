0e32d9d1b54b451c3838db4b6d8dc213edb22d6b
Mokhtar Bacha <mokhtarbacha@Mokhtars-MacBook-Pro.local>
2021-11-29 17:56:04 -0500

    update to version 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_file(filename):
 
 setup(
     name='formal-sqlcommenter',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['tests']),
     extras_require={
         'psycopg2': ['psycopg2'],
