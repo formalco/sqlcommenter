f71b40d09107e00591e30cb8f80f7d00f38c9f01
Mokhtar Bacha <mokhtarbacha@Mokhtars-MacBook-Pro.local>
2021-11-30 16:15:39 -0500

    bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_file(filename):
 
 setup(
     name='formal-sqlcommenter',
-    version='1.0.2',
+    version='1.0.4',
     packages=find_packages(exclude=['tests']),
     extras_require={
         'psycopg2': ['psycopg2'],
