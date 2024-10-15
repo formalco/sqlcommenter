ae2aaa8db2778913c401c7f00c29d7b6a883a58b
ramnes <contact@ramnes.eu>
2024-10-15 12:35:43 +0200

    [ENG-2317] Escape end user id

--- formal/sqlcommenter/__init__.py
+++ formal/sqlcommenter/__init__.py
@@ -13,6 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
+
+if sys.version_info.major <= 2:
+    import urllib
+    url_quote_fn = urllib.quote
+else:
+    import urllib.parse
+    url_quote_fn = urllib.parse.quote
+
 
 def generate_sql_comment(endUserID):
     """
@@ -21,4 +30,15 @@ def generate_sql_comment(endUserID):
     if endUserID == '' or endUserID == None:  # No entries added.
         return ''
 
-    return '/*formal_role_id:{0}*/ '.format(endUserID)
+    return '/*formal_role_id:{0}*/ '.format(url_quote(endUserID))
+
+
+def url_quote(s):
+    if not isinstance(s, (str, bytes)):
+        return s
+    quoted = url_quote_fn(s)
+    # Since SQL uses '%' as a keyword, '%' is a by-product of url quoting
+    # e.g. foo,bar --> foo%2Cbar
+    # thus in our quoting, we need to escape it too to finally give
+    #      foo,bar --> foo%%2Cbar
+    return quoted.replace('%', '%%')
